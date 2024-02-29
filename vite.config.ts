1656f85201b523605e95f7fbf254a5fcef1f531e
Minh Le <minh.le@mgisolutions.com>
2024-02-29 14:47:11 +0700

    add video player

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+// import { vite as vidstack } from 'vidstack/plugins'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  base: "/demo-vue-player"
+    plugins: [vue({
+        template: {
+            compilerOptions: {
+                isCustomElement: (tag) => tag.startsWith('media-'),
+            },
+        },
+    }),
+    // vidstack({ include: /player\// })
+    ],
+    base: "/demo-vue-player"
 })
